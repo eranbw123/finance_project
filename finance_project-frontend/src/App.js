c5229f9d195597902f96fb1e3e8428a821b74ab5
eranbw123 <eranbw123@gmail.com>
2021-12-10 18:57:35 +0200

    working basic frond and backend

--- finance_project-frontend/src/App.js
+++ finance_project-frontend/src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const axios = require("axios");
 
 function App() {
+  const testBackendCall = async () => {
+    const username = "eranbw123";
+    const password = "123123123";
+    const url = "http://127.0.0.1:8000/auth/jwt/create/";
+    // axios.defaults.baseURL = "test";
+    // console.log(url);
+    // const response = await axios.post(url, );
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    const serverResponse = await response.json();
+    console.log(serverResponse.status, serverResponse);
+  };
+
+  useEffect(() => {
+    testBackendCall();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
